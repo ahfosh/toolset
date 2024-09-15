b007583bb8b9eee416340147f1ef453c0ea001e1
ahfosh <3155182425@qq.com>
2024-09-15 23:11:47 +0800

    fix: fix

--- olympic/medal/script.js
+++ olympic/medal/script.js
@@ -31,7 +31,7 @@ function renderTable(data) {
     row.innerHTML = `
             <td>${index + 1}</td>
             <td>${entry.nickname}</td>
-            <td>${entry.real_name || "-"}</td>
+            <td>${entry.name}</td>
             <td>${entry.gold}</td>
             <td>${entry.silver}</td>
             <td>${entry.bronze}</td>
